b61dd5da0506a5ee54d00c556b7a7c6a7b0be26e
vrock11 <madankarvikram@gmail.com>
2021-08-19 18:46:04 +0530

    [Fix] Title of Home Page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Airbnb-clone</title>
+        <title>Airbnb Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
